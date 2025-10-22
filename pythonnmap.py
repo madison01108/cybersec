6e341c78b0a8adfa74c4b7365d96574108bc3b70
Madison <madisonlee01108@gmail.com>
2025-10-21 19:37:46 -0500

    python nmap

---
+++ pythonnmap.py
@@ -0,0 +1,21 @@
+import nmap
+
+#instance of nmap
+
+nm = nmap.portscanner()
+
+#2 targets
+#nmap.org ip address for us to test and use for general public 
+target = "scanme.nmap.org"
+options = "-sV -sC scan_results"
+ 
+nm.scan(target, argument = options)
+
+for host in nm.all_hosts():
+    print("Host: %s (%s)" % (host, nm[host].hostname()))
+    print("State: %s" %nm[host].state())
+    for protocol in nm[host].all_protocols():
+        print("Protocol: %s" % protocol)
+        port_info = nm[host][protocol]
+        for port, state in port_info.items():
+            print("Port: %s\tstate: %s" % (port, state))
